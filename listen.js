3447cab44758e293c1868ba692008207ed5448ef
mike-newh <zq@live.co.uk>
2018-12-07 14:33:27 +0000

    heroku build

--- listen.js
+++ listen.js
@@ -1,7 +1,8 @@
 const app = require('./index.js');
 
+const { PORT = 9090 } = process.env;
 
-app.listen(9090, (err) => {
+app.listen(PORT, (err) => {
   if (err) console.log(err);
   else console.log('listening');
 });
